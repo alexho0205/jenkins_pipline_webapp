7eb5a678c2d4b74d33047819d1ec06d1e2b3c7de
alexho <alexho@gmail.com>
2024-07-14 15:10:56 +0800

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace jenkins_pipline_webapp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
